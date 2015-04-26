4ef516759259b0293d1581b000b527945f211482
teknogeek <jmargolis97@gmail.com>
2015-04-26 12:23:35 -0400

    work on time until

--- platforms/android/assets/www/js/ChurchView.js
+++ platforms/android/assets/www/js/ChurchView.js
@@ -7,7 +7,11 @@ var ChurchView = function(church)
 
 	this.render = function()
 	{
-		this.$el.html(this.template({"churchName": church}));
+		var address = (church == "Avon") ? "590+West+Avon+Rd+Avon+CT+06001" : "718+Pine+St+Bristol+CT+06010";
+		this.$el.html(this.template({
+			"churchName": church,
+			"address": address
+		}));
 		return this;
 	};
 
--- www/js/ChurchView.js
+++ www/js/ChurchView.js
@@ -7,8 +7,23 @@ var ChurchView = function(church)
 
 	this.render = function()
 	{
-		this.$el.html(this.template({"churchName": church}));
+		var address = (church == "Avon") ? "590+West+Avon+Rd+Avon+CT+06001" : "718+Pine+St+Bristol+CT+06010";
+		var timeUntil = moment("4:30 +0000")
+
+		this.$el.html(this.template({
+			"churchName": church,
+			"address": address,
+			"timeUntil" : timeUntil
+		}));
 		return this;
+	};
+
+	this.calcTime = function()
+	{
+		var serviceStartTimes = ["8:00", "9:30", "11:00"];
+		var serviceEndTimes = ["9:00", "10:00"];
+
+		var currentTime = moment().unix();
 	};
 
 	this.initialize();
