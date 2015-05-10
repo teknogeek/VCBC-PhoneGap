f1f34243b6eb7bb7d84183b287a904e84ba9040a
teknogeek <jmargolis97@gmail.com>
2015-05-10 10:53:51 -0400

    time until service

--- www/js/ChurchView.js
+++ www/js/ChurchView.js
@@ -8,7 +8,7 @@ var ChurchView = function(church)
 	this.render = function()
 	{
 		var address = (church == "Avon") ? "590+West+Avon+Rd+Avon+CT+06001" : "718+Pine+St+Bristol+CT+06010";
-		var timeUntil = moment("4:30 +0000")
+		var timeUntil = this.calcTime();
 
 		this.$el.html(this.template({
 			"churchName": church,
@@ -20,10 +20,69 @@ var ChurchView = function(church)
 
 	this.calcTime = function()
 	{
-		var serviceStartTimes = ["8:00", "9:30", "11:00"];
-		var serviceEndTimes = ["9:00", "10:00"];
+	    var sunday = moment().day("Sunday").format("YYYY-MM-DD");
+	    //start times
+	    var serviceStartTimes = [
+	        moment(sunday + " 08:00:00", "YYYY-MM-DD HH:mm:ss").utcOffset("America/New_York"),
+	        moment(sunday + " 09:30:00", "YYYY-MM-DD HH:mm:ss").utcOffset("America/New_York"),
+	        moment(sunday + " 11:00:00", "YYYY-MM-DD HH:mm:ss").utcOffset("America/New_York")
+	    ];
 
-		var currentTime = moment().unix();
+	    //(psuedo) "end" times
+	    var serviceEndTimes = [
+	        moment(sunday + " 09:00:00", "YYYY-MM-DD HH:mm:ss").utcOffset("America/New_York"),
+	        moment(sunday + " 10:00:00", "YYYY-MM-DD HH:mm:ss").utcOffset("America/New_York"),
+	        moment(sunday + " 11:30:00", "YYYY-MM-DD HH:mm:ss").utcOffset("America/New_York")
+	    ];
+
+	    //current date and time
+	    var today = moment().utcOffset("America/New_York");
+
+	    //loop over service start times
+	    for(var timeIndex in serviceStartTimes)
+	    {
+	        //start of service
+	        var service = serviceStartTimes[timeIndex];
+
+	        //end of service
+	        var endOfService = serviceEndTimes[timeIndex];
+
+	        //if current time in between start and "end" times
+	        if(today.isBetween(service, endOfService))
+	        {
+	            //return how long service has been going on for and kill loop
+	            return "started " + service.from(today);
+	            break;
+	        }
+	        
+	        //if not the last service
+	        if(parseInt(timeIndex) + 1 != serviceStartTimes.length)
+	        {   
+	            //if today still before the next service
+	            if(today.isBefore(serviceStartTimes[parseInt(timeIndex) + 1]))
+	            {
+	                //set service to next start time
+	                service = serviceStartTimes[parseInt(timeIndex) + 1];
+
+	                //if today is before the current service, e.g. before 8am
+	                if(today.isBefore(serviceStartTimes[timeIndex]))
+	                {
+	                    //set service to current service start time
+	                    service = serviceStartTimes[timeIndex];
+	                }
+
+	                //return how long until service starts and kill loop
+	                return "starts " + service.from(today);
+	                break;
+	            }
+	        }
+	        else
+	        {
+	            //if past last service "end"
+	            return "starts next week";
+	            break;
+	        }
+	    }
 	};
 
 	this.initialize();
--- www/js/app.js
+++ www/js/app.js
@@ -24,8 +24,6 @@
 
     router.start();
 
-    console.log("Service " + calcTime());
-
     document.addEventListener("deviceready", function()
     {
         StatusBar.overlaysWebView(false);
@@ -46,43 +44,3 @@
         }
     }, false);
 }());
-
-function calcTime()
-{
-    var sunday = moment().day("Sunday").format("YYYY-MM-DD");
-    var serviceStartTimes = [
-        moment(sunday + " 08:00:00", "YYYY-MM-DD HH:mm:ss").utcOffset("America/New_York"),
-        moment(sunday + " 09:30:00", "YYYY-MM-DD HH:mm:ss").utcOffset("America/New_York"),
-        moment(sunday + " 11:00:00", "YYYY-MM-DD HH:mm:ss").utcOffset("America/New_York")
-    ];
-
-    var serviceEndTimes = [
-        moment(sunday + " 09:00:00", "YYYY-MM-DD HH:mm:ss").utcOffset("America/New_York"),
-        moment(sunday + " 10:00:00", "YYYY-MM-DD HH:mm:ss").utcOffset("America/New_York")
-    ];
-
-    //var today = moment().utcOffset("America/New_York");
-    var today = moment().day("Sunday").hour(10).minute(5).second(0);
-
-    for(var time in serviceStartTimes)
-    {
-        var service = serviceStartTimes[time];
-
-        if(time != serviceStartTimes.length - 1)
-        {
-            var endOfService = serviceEndTimes[time];
-
-            if(today.isBetween(service, endOfService))
-            {
-                return "started " + service.from(today);
-                break;
-            }
-            else
-            {
-                service = serviceStartTimes[parseInt(time) + 1];
-                return "starts " + service.from(today);
-                break;
-            }
-        }
-    }
-}
