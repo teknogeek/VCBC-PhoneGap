99569da00809b1b0c8cea6cc2efcbfe7fbb4495d
teknogeek <jmargolis97@gmail.com>
2015-05-17 12:20:07 -0400

    vertical split view

---
+++ www/js/VSplitView.js
@@ -0,0 +1,15 @@
+var VSplitView = function()
+{
+	this.initialize = function()
+	{
+		this.$el = $("<div/>");
+	};
+
+	this.render = function()
+	{
+		this.$el.html(this.template());
+		return this;
+	};
+
+	this.initialize();
+};
